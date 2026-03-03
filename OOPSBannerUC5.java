36dc7d3d8d2645fde1fd14c506e3f756ce4c24d2
Kushagra Kashyap <kushagrakashyap.tech@gmail.com>
2026-03-03 09:43:16 +0530

    UC5 - Implement OOPS Banner using Inline Array Initialization with String.join()

---
+++ OOPSBannerUC5.java
@@ -0,0 +1,49 @@
+public class OOPSBannerUC5 {
+
+    public static void main(String[] args) {
+
+        // Inline array declaration + initialization using String.join()
+        String[] banner = {
+            String.join("  ",
+                    " ***** ",
+                    " ***** ",
+                    " ***** ",
+                    " ******"),
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+            String.join("  ",
+                    "*     *",
+                    "*     *",
+                    "*     *",
+                    "*     *"),
+            String.join("  ",
+                    "*     *",
+                    " ***** ",
+                    " ***** ",
+                    " ******"),
+            String.join("  ",
+                    "*     *",
+                    "*",
+                    "*",
+                    "*     *"),
+            String.join("  ",
+                    "*     *",
+                    "*",
+                    "*",
+                    "*     *"),
+            String.join("  ",
+                    " ***** ",
+                    "*",
+                    "*",
+                    " ******")
+        };
+
+        // Enhanced for-loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
