061ad2ce11720ed3e8399a10c224551645bd234f
Kushagra Kashyap <kushagrakashyap.tech@gmail.com>
2026-03-03 09:55:51 +0530

    UC6: Refactor banner logic using static helper methods for O, P and S

---
+++ OOPSBannerUC6.java
@@ -0,0 +1,61 @@
+public class OOPSBannerUC6 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+                String.join("  ", getO(0), getO(0), getP(0), getS(0)),
+                String.join("  ", getO(1), getO(1), getP(1), getS(1)),
+                String.join("  ", getO(2), getO(2), getP(2), getS(2)),
+                String.join("  ", getO(3), getO(3), getP(3), getS(3)),
+                String.join("  ", getO(4), getO(4), getP(4), getS(4)),
+                String.join("  ", getO(5), getO(5), getP(5), getS(5)),
+                String.join("  ", getO(6), getO(6), getP(6), getS(6))
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+
+    // Helper Method for O
+    public static String getO(int row) {
+        String[] pattern = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+        return pattern[row];
+    }
+
+    // Helper Method for P
+    public static String getP(int row) {
+        String[] pattern = {
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+        return pattern[row];
+    }
+
+    // Helper Method for S
+    public static String getS(int row) {
+        String[] pattern = {
+                " ***** ",
+                "*      ",
+                "*      ",
+                " ***** ",
+                "      *",
+                "      *",
+                " ***** "
+        };
+        return pattern[row];
+    }
+}
